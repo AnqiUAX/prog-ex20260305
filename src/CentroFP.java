8f6963f8fe67bab056cdeeb2e77b52f3d9f5b791
Anqi <anqiwang135@gmail.com>
2026-03-05 09:08:38 +0100

    feet(CentroFP):metodo findAlumno(int idAlumno) creado

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -8,7 +8,19 @@ public class CentroFP {
         alumnos = new Alumno[MAX_ALUMNOS];
     }
 
-    public
+    public Alumno findAlumno(int idAlumno) {
+        boolean seguirBuscar = true;
+        Alumno alumnoEncontrado = null;
+
+        for(int i = 0; i < MAX_ALUMNOS && seguirBuscar; i++){
+            if(alumnos[i] != null && alumnos[i].getId() == idAlumno){
+                alumnoEncontrado = alumnos[i];
+                seguirBuscar = false;
+                alumnoEncontrado = alumnos[i];
+            }
+        }
+        return alumnoEncontrado;
+    }
 
 
 }
